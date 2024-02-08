f8c3ed44be0cb246f56a0bec6d859e986209c781
bpstewar <bstewart@worldbank.org>
2024-02-08 11:56:41 -0500

    testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-#with open("README.md", "r", encoding="utf-8") as fh:
-#    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
  
 long_description = "FUBAR"
  
