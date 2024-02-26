0baf500da9b391c3023c688fb06d9aad69b08588
Gabriel Stefanini Vicente <gvicente@worldbank.org>
2024-02-26 12:16:09 -0500

    Add pre-commit (#14)

--- notebooks/Implementations/Slum_Mapping/slumML/STEP2.py
+++ notebooks/Implementations/Slum_Mapping/slumML/STEP2.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import os, sys, time
+import os
+import time
 import pandas as pd
 import geopandas as gpd
 import h2o
@@ -14,22 +15,20 @@ start_time = time.time()
 
 ### Initial file setting --------------------------------------------------------------------
 pth = os.getcwd()
-building_file = '/Niamey_data/buildings_altered.shp'
-sample_file = '/Niamey_data/Niamey_sample_data.shp'
+building_file = "/Niamey_data/buildings_altered.shp"
+sample_file = "/Niamey_data/Niamey_sample_data.shp"
 
 # Read a processed Building Footprint layer
 building_df = gpd.read_file(pth + building_file)
-building_df = building_df.to_crs({'init':'epsg:4326'})
+building_df = building_df.to_crs({"init": "epsg:4326"})
 
 # Read a Sample Area layer
 sample_area = gpd.read_file(pth + sample_file)
-sample_area = sample_area.to_crs({'init':'epsg:4326'})
+sample_area = sample_area.to_crs({"init": "epsg:4326"})
 
 # Urban classes to be used in the sample layer and for classification
 # Assign unique integer for each class by yourself here.
-class_map = {'middle income':1,'informal':2,'formal':3, 'commercial':4}
-
-
+class_map = {"middle income": 1, "informal": 2, "formal": 3, "commercial": 4}
 
 
 ### Variable prep here ----------------------------------------------------------------------
@@ -38,9 +37,7 @@ class_map = {'middle income':1,'informal':2,'formal':3, 'commercial':4}
 # Current setting is very basic: Apply all variables in the building_df.
 col = building_df.columns
 predictors = list(col[1:21])
-response = 'type'
-
-
+response = "type"
 
 
 ### Generate a training data by intersecting 'building_df' and 'sample_area'-----------------
@@ -48,24 +45,22 @@ response = 'type'
 
 source_df = building_df.copy()
 
-source_df['type'] = 'unknown'
+source_df["type"] = "unknown"
 
 # Create an empty DF for append
 training_data = pd.DataFrame()
 
-# 'training_data' is now our official 'training data' for the ML model. 
+# 'training_data' is now our official 'training data' for the ML model.
 for index, row in sample_area.iterrows():
     x = row.geometry
     y = row.type
-    
+
     df_temp = source_df[source_df.intersects(x)].copy()
-    df_temp['type'] = y
-    
+    df_temp["type"] = y
+
     training_data = training_data.append(df_temp)
-    
-training_data['type'] = training_data['type'].map(class_map)
 
-
+training_data["type"] = training_data["type"].map(class_map)
 
 
 ### Model training here ---------------------------------------------------------------------
@@ -81,7 +76,7 @@ hf = H2OFrame(training_data)
 # whichever is first, it returns a DataFrame of predictions as preds, ordered by the quality of their predictions.
 
 # Split 'hf' into a taraining frame and validation frame.
-train, valid = hf.split_frame(ratios = [.8], seed = 10)
+train, valid = hf.split_frame(ratios=[0.8], seed=10)
 
 # Identify predictors and response
 x = predictors
@@ -92,10 +87,8 @@ train[y] = train[y].asfactor()
 valid[y] = valid[y].asfactor()
 
 # Run AutoML for 20 base models (limited to 1 hour max runtime by default)
-aml = H2OAutoML(max_models = 20, seed =1)
-aml.train(x = x,
-          y = y,
-          training_frame = train)
+aml = H2OAutoML(max_models=20, seed=1)
+aml.train(x=x, y=y, training_frame=train)
 
 # View the AutoML Leaderboard
 lb = aml.leaderboard
@@ -113,11 +106,7 @@ res = aml.leader.model_performance(valid)
 print(res)
 
 # We save the model down to its own save location.
-model_path = h2o.save_model(model = aml.leader,
-                            path = pth,
-                            force = True)
-
-
+model_path = h2o.save_model(model=aml.leader, path=pth, force=True)
 
 
 ### Model fitting here ----------------------------------------------------------------------
@@ -131,74 +120,70 @@ max_row_size = 100000
 chunk_num = int(len(building_df) / max_row_size)
 chunk_mod = len(building_df) % max_row_size
 
-building_df['type'] = 'unknown'
+building_df["type"] = "unknown"
 
 
 def MLpred(df):
     df_input = df[predictors]
     # Extract predictor cols only (specified by the 'predictors' LIST)
     hf_temp = H2OFrame(df_input)
-    
+
     preds_temp = aml.leader.predict(hf_temp)
     pred_df_temp = preds_temp.as_data_frame()
-    
+
     # add 'PID' to 'pred_df_temp' so that it will be merged to the original 'df.'
-    df.reset_index(inplace = True)
-    pred_df_temp['PID'] = df.PID
-    
-    ans = pd.merge(df, pred_df_temp, on = "PID")
-    
-    return(ans)
-    
+    df.reset_index(inplace=True)
+    pred_df_temp["PID"] = df.PID
+
+    ans = pd.merge(df, pred_df_temp, on="PID")
+
+    return ans
+
 
 # Create an empty DF for append
 prediction_df = pd.DataFrame()
 
 for i in range(0, chunk_num):
     if i == 0:
-        print("Processing Chunk No. {} ----> row 0–{}".format(i+1, max_row_size))
+        print("Processing Chunk No. {} ----> row 0–{}".format(i + 1, max_row_size))
         df_temp2 = building_df[0:max_row_size].copy()
-        
+
         # Prediction process here
         pred_x = MLpred(df_temp2)
-        
+
         prediction_df = prediction_df.append(pred_x)
-        
+
     else:
-        start = (i * max_row_size)
-        stop =  (i * max_row_size) + max_row_size
-        print("Processing Chunk No. {} ----> row {}–{}".format(i+1, start, stop))
+        start = i * max_row_size
+        stop = (i * max_row_size) + max_row_size
+        print("Processing Chunk No. {} ----> row {}–{}".format(i + 1, start, stop))
         df_temp2 = building_df[start:stop].copy()
 
         # Prediction process here
         pred_x = MLpred(df_temp2)
-        
-        prediction_df = prediction_df.append(pred_x)            
-    
+
+        prediction_df = prediction_df.append(pred_x)
+
 if chunk_mod > 0:
-    start = (chunk_num * max_row_size)
-    print("Processing Chunk No. {} ----> row {} till the end".format(i+1, start))    
+    start = chunk_num * max_row_size
+    print("Processing Chunk No. {} ----> row {} till the end".format(i + 1, start))
     df_temp2 = building_df[start:].copy()
-    
+
     # Prediction process here
     pred_x = MLpred(df_temp2)
 
     prediction_df = prediction_df.append(pred_x)
 
 
-
-
 ### Exporting -------------------------------------------
-print('Exporting reulst to shapefile...')
-output_path = pth + '\prediction_result.shp'
-prediction_df.to_file(output_path, driver = 'ESRI Shapefile')
-
-
+print("Exporting reulst to shapefile...")
+output_path = pth + "\prediction_result.shp"
+prediction_df.to_file(output_path, driver="ESRI Shapefile")
 
 
 ### Refreshing H2O cluster (if necessary) -------------------------------------------
 
 elapsed_time = (time.time() - start_time) / 60
-print ("elapsed_time:{0}".format(elapsed_time) + "[Min]")
+print("elapsed_time:{0}".format(elapsed_time) + "[Min]")
 
 h2o.cluster().shutdown(prompt=True)
